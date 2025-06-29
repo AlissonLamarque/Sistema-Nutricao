5efc2783baad6408f192f607309e2fd499d62a56
Bernardo Paz de Freitas <2004be@gmail.com>
2025-06-29 15:48:42 -0300

    commit

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "=== Limpando recursos Kubernetes ==="
-
-# Deletar namespace (isso remove tudo dentro dele)
-kubectl delete namespace nutricao
-
-echo "=== Limpeza concluída ==="
-echo "Todos os recursos do namespace 'nutricao' foram removidos" 
